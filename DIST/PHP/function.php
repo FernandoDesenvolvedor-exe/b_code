8d5905a8ae2e293caa94dd6f5496c770c9c4cf5b
Daniel54gomes <daniel54henrich@gmail.com>
2023-10-11 20:07:54 -0300

    a

---
+++ DIST/PHP/function.php
@@ -0,0 +1,5 @@
+<?php 
+    include('validaLogin.php');
+    include('validaCadastro.php');
+    
+?>
--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,5 @@
 <?php
-
+    include('function.php');
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
