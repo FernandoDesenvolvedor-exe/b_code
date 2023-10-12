7e49356af7d7ac8453d70c7370edf832d712078c
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-10-11 22:00:33 -0300

    atualização

--- php/connection.php
+++ php/connection.php
@@ -1,5 +1,5 @@
 <?php
-                //ip/local - login - senha - nome database 
+                //localhost - loginhost - senhahost - nome do banco de dados 
     $conn = mysqli_connect("localhost","root","","lab_plasticos")
             or die("Error: ".mysqli_connect_error());       
 ?>
