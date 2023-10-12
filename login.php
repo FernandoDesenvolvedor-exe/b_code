3ffeda914d293f1b547e615ed981629e57133df9
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-10-11 21:35:57 -0300

    atualização

--- login.php
+++ login.php
@@ -55,7 +55,7 @@
                             <br>
                         </div>
                         <!-- Form -->
-                        <form class="form-horizontal m-t-20" id="loginform" method="POST" action="php/validaLogin.php">
+                        <form class="form-horizontal m-t-20" id="loginform" method="POST" action="">
                             <div class="row p-b-30">
                                 <div class="col-12">
                                     <div class="input-group mb-3">
