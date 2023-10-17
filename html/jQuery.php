3d2517ea86bb52b1e2b925124fe73bdbda1354a9
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-10-17 20:20:32 -0300

    Atualizar

---
+++ html/jQuery.php
@@ -0,0 +1,20 @@
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="dist/assets/libs/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="dist/assets/libs/popper.js/dist/umd/popper.min.js"></script>
+<script src="dist/assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="dist/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+<script src="dist/assets/extra-libs/sparkline/sparkline.js"></script>
+<!--Wave Effects -->
+<script src="dist/js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="dist/js/sidebarmenu.js"></script>
+<!--Custom JavaScript -->
+<script src="dist/js/custom.min.js"></script>
+<!-- this page js -->
+<script src="dist/assets/extra-libs/multicheck/datatable-checkbox-init.js"></script>
+<script src="dist/assets/extra-libs/multicheck/jquery.multicheck.js"></script>
+<script src="dist/assets/extra-libs/DataTables/datatables.min.js"></script>
