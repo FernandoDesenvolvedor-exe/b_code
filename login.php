b6ce4c922adcb822193e0729f15078e73dc3a5be
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-10-17 21:57:40 -0300

    Atualização

--- login.php
+++ login.php
@@ -4,51 +4,18 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>        
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="Luis Fernando Pereira">
-        <!-- icone senai 16x16 -->
-        <link rel="icon" type="image/png" sizes="16x16" href="dist/assets/images/logoLabPlasticos16x16.png">
-        <!-- Custom CSS -->
-        <link href="dist/css/style.min.css" rel="stylesheet">   
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-        <title>Login</title>
-        <style>
-            body{
-                background-color: light aqua important;
-            }
-        </style>
-    </head>
+
+    <?php include('pages/cabecalho.php');?>
 
     <body>
             <!-- ============================================================== -->
             <!-- Login box.scss -->
             <!-- ============================================================== -->
         <div class="main-wrapper">
-            <!-- ============================================================== -->
-            <!-- Preloader - style you can find in spinners.css -->
-            <!-- ============================================================== -->
-            <div class="preloader">
-                <div class="lds-ripple">
-                    <div class="lds-pos"></div>
-                    <div class="lds-pos"></div>
-                </div>
-            </div>
-            <!-- ============================================================== -->
-            <!-- Preloader - style you can find in spinners.css -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- Login box.scss -->
-            <!-- ============================================================== -->
+            
+            <?php include('pages/preloader.php');?> 
+            
+            <!-- Login box.scss -->            
             <div class="auth-wrapper d-flex no-block justify-content-center bg-cyan align-items-center">
                 <div class="auth-box bg-blue border-top border-secondary">
                     <div id="loginform">
