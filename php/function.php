84f013633a4239ccf9345ca754bfccd033b8b091
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-10-25 21:08:41 -0300

    atualização

--- php/function.php
+++ php/function.php
@@ -1,19 +1,11 @@
 <?php    
     include('functionAlerts.php');
-<<<<<<< Updated upstream
     include('functionBanco.php');
     include('functionMaquinario.php');
     include('functionMateriais.php');
     include('functionProduto.php');
     include('functionTurmas.php');
-    include('saveMateriais.php');
-    include('saveUsuario.php');
-    include('validaDados.php');
-    include('validaLogin.php');
-    
-=======
-    include('functionProduto.php')
-
->>>>>>> Stashed changes
+    include('functionUsuario.php');
+    include('functionProduto.php');
 ?>
 
