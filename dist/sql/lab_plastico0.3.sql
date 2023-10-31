8eac7a7dfb7e2d2122f27a0a8b8359191649fd69
Daniel54gomes <daniel54henrich@gmail.com>
2023-10-31 19:29:12 -0300

    a

--- dist/sql/lab_plastico0.3.sql
+++ dist/sql/lab_plastico0.3.sql
@@ -1,22 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 31-Out-2023 às 22:58
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+drop database lab_plasticos;
+create database lab_plasticos;
+use lab_plasticos;
 --
 -- Banco de dados: `lab_plasticos`
 --
@@ -378,10 +362,20 @@ CREATE TABLE `usuarios` (
   `senha` varchar(32) NOT NULL COMMENT 'Registra a senha de um usuário',
   `nome` varchar(80) NOT NULL COMMENT 'Registra o primeiro nome do usuário',
   `sobrenome` varchar(80) NOT NULL COMMENT 'Registra o sobrenome do usuário',
-  `idTurma` int(11) NOT NULL COMMENT 'FK - chave estrangeira que Registra a ID da turma do usuário',
+  `idTurma` int(11) NULL COMMENT 'FK - chave estrangeira que Registra a ID da turma do usuário',
   `tipo` tinyint(1) NOT NULL COMMENT 'Registra o tipo de usuário',
   `ativo` tinyint(1) NOT NULL COMMENT 'Registra se o usuário estiver ativo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabela que registra os cadastros de suuarios';
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `login`, `senha`, `nome`, `sobrenome`, `idTurma`, `tipo`, `ativo`) VALUES
+(1, 'adm@adm.com', '202cb962ac59075b964b07152d234b70', 'Adm', 'Adm', null, 1, 1),
+(2, 'a@teste.com', '202cb962ac59075b964b07152d234b70', 'Luis', 'Fernando Pereira', null, 1, 1),
+(3, 'b@teste.com', '202cb962ac59075b964b07152d234b70', 'Marco', 'dos Santos', 1, 2, 1),
+(4, 'c@teste.com', '202cb962ac59075b964b07152d234b70', 'Teste', 'Testudo de teste', 1, 2, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -519,7 +513,7 @@ ALTER TABLE `turma`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`idUsuario`),
-  ADD INDEX KEY(`idTurma`);
+  ADD INDEX (`idTurma`);
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
@@ -612,7 +606,7 @@ ALTER TABLE `turma`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK - chave identificadora de IDs da tabela usuarios';
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK - chave identificadora de IDs da tabela usuarios', AUTO_INCREMENT=4;
 
 --
 -- Restrições para despejos de tabelas
@@ -687,7 +681,3 @@ ALTER TABLE `receitas`
   ADD CONSTRAINT `receitas_ibfk_1` FOREIGN KEY (`idProduto`) REFERENCES `produtos` (`idProduto`),
   ADD CONSTRAINT `receitas_ibfk_2` FOREIGN KEY (`idMateriaPrima`) REFERENCES `materia_prima` (`idMateriaPrima`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
