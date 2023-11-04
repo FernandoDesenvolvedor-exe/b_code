f99a626ed71d749aace1e791345aecb558440682
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-11-04 19:40:40 -0300

    atualização

--- php/saveprodutos.php
+++ php/saveprodutos.php
@@ -71,52 +71,136 @@
             mysqli_close($conn);
         }
 
-    }else if ($validacao == 'UPF') {
+    }else if ($validacao == 'UPF') { // update de um produto 
 
-        $idProduto = $_GET['idProduto'];
-        $idFeramental = $_GET['idFerramental'];
         $descPdto = stripslashes($_POST['nProduto']);
-        $descFerr = stripslashes($_POST['nMolde']);
-        $idTipoFerr = $_POST['nTipoFerramental'];
-        $qtde = $_POST['nQtd'];
+        $descFerr = stripslashes($_POST['nMolde']);    
 
-        $arrayProduto = array($descPdto,$qtde,$idProduto);
+        if (isset($_POST['nProduto']) == true && $_POST['nProduto'] != ""){  //update da tabela de produtos
 
-        validaBanco($arrayTabela);
-        
-        $sql = "UPDATE produtos SET descricao=".$descPdto.", peso=".$qtde.")"
-        ."VALUES('".$descPdto."',".$qtde.", 1);";
+            if (isset($_POST['nQtd']) == true && $_POST['nQtd'] != ""){
 
-        $result = mysqli_query($conn, $sql);        
+                $sql = 'UPDATE produtos' 
+                        .' SET peso = '.$_POST['nQtd'].','
+                        .' descricao = "'.$descPdto.'"'
+                        .' WHERE idProduto = '.$_GET['idProduto'].';';
 
-        //Traz o ultimo id da tabela enviada nos parametros. Enviar nome da tabela e nome da coluna id 
-        $idPdto = buscaId("produtos","idProduto");
+                $result = mysqli_query($conn, $sql);
+    
+            } else{
 
-        $sql = "INSERT INTO ferramental (descricao, ativo, idTiposFerramental, idProduto)"
-                ." VALUES ('".$descFerr."', 1, ".$idTipoFerr.",".$idPdto.");";
+                $sql = 'UPDATE produtos'
+                        .' SET descricao = "'.$descPdto.'"'
+                        .' WHERE idProduto = '.$_GET['idProduto'].';';                
 
-        $result = mysqli_query($conn, $sql);
+                $result = mysqli_query($conn, $sql);
 
-        $idFerr = buscaId('ferramental', 'idFerramental');
+            }
 
-        for($i = 0; $i < count($_POST['nMaquina']); $i++){
+        } else{
 
-            $sql = "INSERT INTO ferramental_maquina(idFerramental, idMaquina)" 
-                    ."VALUES(".$idFerr.", ".$_POST['nMaquina'][$i].");";
+            if (isset($_POST['nQtd']) == true && $_POST['nQtd'] != ""){
+                
+                $sql = 'UPDATE produtos' 
+                .' SET peso = '.$_POST['nQtd'].''
+                .' WHERE idProduto = '.$_GET['idProduto'].';';
 
-            $result = mysqli_query($conn, $sql);
+                $result = mysqli_query($conn, $sql);
+            
+            }
         }
 
+        if (isset($_POST['nMolde']) == true && $_POST['nMolde'] != ""){
+
+            if (isset($_POST['nTipoFerramental']) == true && $_POST['nTipoFerramental'] != ""){
+
+                $sql = 'UPDATE ferramental'
+                .' SET idTiposFerramental = '.$_POST['nTipoFerramental'].','
+                .' descricao = "'.$descFerr.'"'
+                .' WHERE idFerramental = '.$_GET['idFerramental'].';'; 
+
+                $result = mysqli_query($conn, $sql);  
+    
+            }else {
+
+                $sql = 'UPDATE ferramental'
+                .' SET descricao = "'.$descFerr.'"'
+                .' WHERE idFerramental = '.$_GET['idFerramental'].';';
+
+                $result = mysqli_query($conn, $sql);
+            }
+        
+        } else{
+
+            if (isset($_POST['nTipoFerramental']) == true && $_POST['nTipoFerramental'] != ""){
+
+                $sql = 'UPDATE ferramental'
+                .' SET idTiposFerramental = '.$_POST['nTipoFerramental'].''
+                .' WHERE idFerramental = '.$_GET['idFerramental'].';';
+
+                $result = mysqli_query($conn, $sql);
+    
+            }
+        }            
+        
         mysqli_close($conn);
+
+        //VERIFICA SE TEM IMAGEM NO INPUT
+        if(isset($_FILES['nImagem']['tmp_name']) != ""){
+
+            //Usar o mesmo nome do arquivo original
+            //$nomeArq = $_FILES['Foto']['name'];
+            //...
+            //OU
+            //Pega a extensão do arquivo e cria um novo nome pra ele (MD5 do nome original)
+            $extensao = pathinfo($_FILES['nImagem']['name'], PATHINFO_EXTENSION);
+            $novoNome = md5($_FILES['nImagem']['name']).'.'.$extensao;        
+            
+            //Verificar se o diretório existe, ou criar a pasta
+            if(is_dir('../dist/img/')){
+                //Existe
+                $diretorio = '../dist/img/';
+            }else{
+                //Criar pq não existe
+                $diretorio = mkdir('../dist/img/');
+            }
+
+            //Cria uma cópia do arquivo local na pasta do projeto
+            move_uploaded_file($_FILES['nImagem']['tmp_name'], $diretorio.$novoNome);
+
+            //Caminho que será salvo no banco de dados
+            $dirImagem = 'dist/img/'.$novoNome;
+
+            include("connection.php");
+            //UPDATE
+            $sql = "UPDATE produtos "
+                    ." SET imagem = '".$dirImagem."'"
+                    ." WHERE idProduto = ".$_GET['idProduto'].";";
+                    
+            $result = mysqli_query($conn,$sql);
+            mysqli_close($conn);
+
+        }     
+
+        header('location:../produtos.php');
 
     } else if ($validacao == 'DPF'){
 
         $idProduto = $_GET['idProduto'];
 
-        $sql="UPDATE ferramental SET ativo= 0 WHERE idProduto=".$idProduto.";";
+        $sql="UPDATE produtos SET ativo= 0 WHERE idProduto=".$idProduto.";";
 
         $result = mysqli_query($conn,$sql);
         mysqli_close($conn);
+
+    } else if ($validacao == 'ITF'){ // Insert um cadastro de um tipo de ferramental
+        $descricao = stripslashes($_POST['nTipoMolde']);
+
+        $sql = "INSERT INTO tipos_ferramental(descricao, ativo) VALUES ('".$descricao."', 1);";
+
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        
     }
 
     header('location: ../produtos.php');
