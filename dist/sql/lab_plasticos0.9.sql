2e16119c5751b228d42cb93a9c8710754c37a444
Daniel54gomes <daniel54henrich@gmail.com>
2023-11-13 20:25:48 -0300

    a

--- dist/sql/lab_plasticos0.9.sql
+++ dist/sql/lab_plasticos0.9.sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 10-Nov-2023 às 00:43
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+drop database lab_plasticos;
+create database lab_plasticos;
+use lab_plasticos;
 
 --
 -- Banco de dados: `lab_plasticos`
@@ -814,7 +799,3 @@ ALTER TABLE `receita_materia_prima`
   ADD CONSTRAINT `receita_materia_prima_ibfk_1` FOREIGN KEY (`idMateriaPrima`) REFERENCES `materia_prima` (`idMateriaPrima`),
   ADD CONSTRAINT `receita_materia_prima_ibfk_2` FOREIGN KEY (`idReceita`) REFERENCES `receitas` (`idReceita`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
