0cf3efc54a5e6a317f36bd973088b9ea53dcd43f
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2023-11-30 22:11:35 -0300

    atualização

--- php/ajaxConnection.php
+++ php/ajaxConnection.php
@@ -1,21 +1,21 @@
 <?php 
 //conexao com bd local
-/*$sql_details = array(
+$sql_details = array(
     'host' => 'localhost',
     'user' => 'root',
     'pass' => '',
     'db'   => 'lab_plasticos'
-);*/
+);
 
 
 //Conexao com BD no servidor
 
-$sql_details = array(
+/*$sql_details = array(
     'host' => 'minha-sa.com.br:3306',
     'user' => 'u638013300_labplasticos',
     'pass' => 'Labplasticos@2023',
     'db'   => 'u638013300_labplasticos'
-);
+);*/
 
 
 ?>
