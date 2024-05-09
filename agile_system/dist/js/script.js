0ce59afed31221bcf0772b4fb5e1301eff37c85b
Luis Fernando Pereira <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2024-05-09 18:33:30 -0300

    atualização de css do menu-sidebar
    mudanças de backgroundColor e padding

--- agile_system/dist/js/script.js
+++ agile_system/dist/js/script.js
@@ -3,3 +3,11 @@ const menu = document.querySelector('#toggle-btn');
 menu.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+document.getElementById("menu-dropdown").addEventListener('click', function(){
+    
+});
+
+
+
+
