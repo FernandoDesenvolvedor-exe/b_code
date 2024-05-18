266a8a9d291e18f1b526e99c09095a39c19d4427
Luis Fernando Pereira <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2024-05-17 21:36:08 -0300

    Add files via upload

--- agile_system/dist/js/script.js
+++ agile_system/dist/js/script.js
@@ -11,20 +11,23 @@ var janela = width;
 
 menu.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("expand");
+    var menu = document.getElementsByClassName('sidebar-item');
 
     if(document.querySelector('#sidebar').classList.contains("expand")){
 
-        if(width >= (janela*0.50)){
-            document.getElementById('main').style.left = width*0.20+'px';
-            document.getElementById('main').style.width = width*0.80+'px';
+
+        for(i=0; i<menu.length; i++) {
+            menu[i].style.visibility = 'hidden';
         }
 
+        document.getElementById('main').style.left = width*0.20+'px';
+        document.getElementById('main').style.width = width*0.80+'px';  
     } else {
 
-        if(width >= (janela*0.50)){
-            document.getElementById('main').style.left = width*0.05+'px';
-            document.getElementById('main').style.width = width*0.95+'px';
+        for(i=0; i<menu.length; i++) {
+            menu[i].style.visibility = 'visible';
         }
-    }
-        
+        document.getElementById('main').style.left = width*0.05+'px';
+        document.getElementById('main').style.width = width*0.95+'px';
+    }        
 });
