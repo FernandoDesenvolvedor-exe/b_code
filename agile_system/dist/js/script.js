3c2d8dab58f173bc19948f553837dd4eba94c55d
Luis Fernando Pereira <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2024-05-17 21:04:23 -0300

    Correção de responsividade

--- agile_system/dist/js/script.js
+++ agile_system/dist/js/script.js
@@ -1,13 +1,30 @@
 const menu = document.querySelector('#toggle-btn');
+const height = window.innerHeight;
+const width = window.innerWidth;
+
+var janela = width;
+
+
+
+
+
 
 menu.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("expand");
+
+    if(document.querySelector('#sidebar').classList.contains("expand")){
+
+        if(width >= (janela*0.50)){
+            document.getElementById('main').style.left = width*0.20+'px';
+            document.getElementById('main').style.width = width*0.80+'px';
+        }
+
+    } else {
+
+        if(width >= (janela*0.50)){
+            document.getElementById('main').style.left = width*0.05+'px';
+            document.getElementById('main').style.width = width*0.95+'px';
+        }
+    }
+        
 });
-
-document.getElementById("#menu-dropdown").addEventListener('click', function(){
-    alert('ioioi');
-});
-
-
-
-
