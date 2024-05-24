fc241fa7472f276676fef554a55dfa250c83302c
Luis Fernando Pereira <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2024-05-24 16:30:49 -0300

    atualização de menu
    Adicionada responsividade e
    Imagem de fundo

--- agile_system/dist/js/script.js
+++ agile_system/dist/js/script.js
@@ -11,23 +11,12 @@ var janela = width;
 
 menu.addEventListener("click", function(){
     document.querySelector("#sidebar").classList.toggle("expand");
-    var menu = document.getElementsByClassName('sidebar-item');
+    const menu = document.getElementsByClassName('sidebar-item');
+    const main = document.getElementById('principal');
 
     if(document.querySelector('#sidebar').classList.contains("expand")){
-
-
-        for(i=0; i<menu.length; i++) {
-            menu[i].style.visibility = 'hidden';
-        }
-
-        document.getElementById('main').style.left = width*0.20+'px';
-        document.getElementById('main').style.width = width*0.80+'px';  
+        main.style.marginLeft = '20%';
     } else {
-
-        for(i=0; i<menu.length; i++) {
-            menu[i].style.visibility = 'visible';
-        }
-        document.getElementById('main').style.left = width*0.05+'px';
-        document.getElementById('main').style.width = width*0.95+'px';
+        main.style.marginLeft = '5%';
     }        
 });
