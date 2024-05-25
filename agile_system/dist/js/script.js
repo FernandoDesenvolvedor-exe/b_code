b47c2844d2b2b7a8c5d5b3e7b50987e62e55d127
FernandoDesenvolvedor-exe <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2024-05-25 13:36:29 -0300

    atualização menu

--- agile_system/dist/js/script.js
+++ agile_system/dist/js/script.js
@@ -1,21 +1,33 @@
 const menu = document.querySelector('#toggle-btn');
 const main = document.getElementById('principal');
+const dropdown = document.getElementById('menu-dropdown');
+const janela = window.innerWidth;
 
 if(sessionStorage.getItem('menuOpen') == 'true'){
     document.querySelector("#sidebar").classList.add("expand");    
     main.style.marginLeft = '20%';
 }
 
-menu.addEventListener("click", function(){
-    document.querySelector("#sidebar").classList.toggle("expand");
-    
+if(sessionStorage.getItem('menu_dropdown') == 'true'){
+    dropdown.classList.remove('collapsed');
+    dropdown.setAttribute('aria-expanded', 'true');
+    document.querySelector('#auth').classList.add('show');
+}
+
+dropdown.addEventListener('click', function(){
+    sessionStorage.setItem('menu_dropdown', dropdown.getAttribute('aria-expanded'));
+});
+
+menu.addEventListener("click", function(event){
+    document.querySelector("#sidebar").classList.toggle("expand");    
+        
     if(document.querySelector('#sidebar').classList.contains("expand")){
-        main.style.marginLeft = '20%';
+        main.style.marginLeft = '20%';            
         sessionStorage.setItem('menuOpen', true);
-    } else {
-        main.style.marginLeft = '5%';
+    } else {  
+        main.style.marginLeft = '5%';   
         sessionStorage.setItem('menuOpen', false);
-    }        
+    }      
 });
 
 function ativaItem(element){    
